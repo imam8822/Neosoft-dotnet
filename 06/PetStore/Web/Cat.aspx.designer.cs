29b6d907bee6444324e287f7e822d936582513fe
kaurrevature <33356218+kaurrevature@users.noreply.github.com>
2021-09-06 14:21:38 +0530

    removinf validations

--- 06/PetStore/Web/Cat.aspx.designer.cs
+++ 06/PetStore/Web/Cat.aspx.designer.cs
@@ -15,15 +15,6 @@ namespace Web
     {
 
         /// <summary>
-        /// ValidationSummary1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ValidationSummary ValidationSummary1;
-
-        /// <summary>
         /// lbl_Name control.
         /// </summary>
         /// <remarks>
@@ -42,15 +33,6 @@ namespace Web
         protected global::System.Web.UI.WebControls.TextBox tb_Name;
 
         /// <summary>
-        /// RequiredFieldValidator1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
-
-        /// <summary>
         /// lbl_Dob control.
         /// </summary>
         /// <remarks>
@@ -67,15 +49,6 @@ namespace Web
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox tb_Dob;
-
-        /// <summary>
-        /// RangeValidator1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RangeValidator RangeValidator1;
 
         /// <summary>
         /// lbl_Gender control.
