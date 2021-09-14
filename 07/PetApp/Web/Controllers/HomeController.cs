2e8cfb38c8abc2a6c1df601ddd5e3edc4918ed06
kaurrevature <33356218+kaurrevature@users.noreply.github.com>
2021-09-14 15:16:54 +0530

    Asp.net MVC app

---
+++ 07/PetApp/Web/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello";
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "About View";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
