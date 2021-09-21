12f1c2c92e02ba74116bea5b941c0bb35e0e6088
kaurrevature <33356218+kaurrevature@users.noreply.github.com>
2021-09-21 12:09:34 +0530

    Rest API

---
+++ 07/PetApp/RestService/Controllers/PetController.cs
@@ -0,0 +1,19 @@
+﻿using RestService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RestService.Controllers
+{
+    public class PetController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Cat> Get()
+        {
+            return Cat.getCats();
+        }
+    }
+}
---
+++ 08/Notes.md
@@ -0,0 +1,1 @@
+## [REST API](https://restfulapi.net/)
